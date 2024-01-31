5eee08ebae6971f7b6c7ab16b6ae5d71dc007598
Lucius Kwok <lk@felttip.com>
2024-01-30 23:05:36 -0500

    Initial Commit

---
+++ SDL_Xcode/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  SDL_Xcode
+//
+//  Created by Lucius Kwok on 1/30/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	printf("Hello, World!\n");
+	return 0;
+}
